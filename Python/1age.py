1a0ec0ec8b676459a40a34598876a2485571b9a0
RAJESHBOYA8 <boyarajesh888@gmail.com>
2025-07-09 12:37:33 +0530

    Print age

---
+++ Python/1age.py
@@ -0,0 +1,5 @@
+def printage(name,age):
+    print("Hello " + name + ", You will be ",age+5," years old in 5 years")
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+printage(name,age)
