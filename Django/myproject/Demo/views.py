030296f6eaa4a6cc1f9e2f553912d7c5ad3e279a
Boya Rajesh <boyarajesh888@gmail.com>
2025-07-11 15:57:57 +0530

    Added full Django project structure including Demo app and templates

---
+++ Django/myproject/Demo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+
+# Create your views here.
+def home_view(request):
+    name="Rajesh"
+    return render(request, 'HTML/home.html',{'name':name})
+def about_view(request):
+    return render(request, 'HTML/about.html')
+def contact_view(request):
+    name=request.GET.get('name')
+    return render(request, 'HTML/home.html',{'name':name})
+def name_view(request,name):
+    return render(request, 'HTML/home.html',{'name':name})
+def http_resp(request):
+    return HttpResponse('<html><body><h1>Welcome Back</h1></body></html>')
+def json_data(request):
+    data={'name': 'Rajesh'}
+    return JsonResponse(data)
